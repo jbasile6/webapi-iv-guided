f9eef579c33a057c9f33c9946d0a3328b8b7ea92
James Basile <jbasile145@gmail.com>
2019-03-21 13:50:03 -0400

    debug index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 4000;
 //makes the port dynamic^^^
 const greeting = process.env.GREETING;
 
-server.listen(4000, () => {
+server.listen(port, () => {
 
   console.log(`\n***  ${greeting} Server Running on http://localhost:${port} ***\n`);
 });
