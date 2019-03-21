82a055b82fc6566515f47441bf2dcf9b0b224a38
James Basile <jbasile145@gmail.com>
2019-03-21 13:36:22 -0400

    moved variables outside of func

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ require('dotenv').config();
 //^^^^imports the .env file variables
 const server = require('./api/server.js');
 
+const port = process.env.PORT || 4000;
+//makes the port dynamic^^^
+const greeting = process.env.GREETING;
+
 server.listen(4000, () => {
-  const port = process.env.PORT || 4000;
-  //makes the port dynamic^^^
-  const greeting = process.env.GREETING;
+
   console.log(`\n***  ${greeting} Server Running on http://localhost:${port} ***\n`);
 });
